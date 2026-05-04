3d364fe0a903448495e0d9e52ef4cf033e316400
zzaain <sheriellaghzain@gmail.com>
2026-05-04 11:58:12 +0700

    menambahkan definisi constructor dengan parameter

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -8,6 +8,13 @@ public:
 };
 
 
+// Definisi constructor dengan parameter
+mahasiswa::mahasiswa(int nim, string nama) {
+    cout << "Constructor Dengan Parameter Terpanggil" << endl;
+    cout << "NIM : " << nim << endl;
+    cout << "Nama : " << nama << endl;
+}
+
 int main(){
     
 }
