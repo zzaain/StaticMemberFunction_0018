e8ba5219c8acd42fcddaf37df4ae9d8fdd7afe31
zzaain <sheriellaghzain@gmail.com>
2026-05-04 11:47:50 +0700

    menambahkan class mahasiswa dan constructor

---
+++ constructor.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa {
+public:
+    mahasiswa(); // Constructor
+};
+
+mahasiswa::mahasiswa() {
+    cout << "Constructor Terpanggil" << endl; // Definisi Constructor
+};
+
+int main() {
+   
+}
