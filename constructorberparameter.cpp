76af2fe7642366797b51098797a0364128c5c48b
zzaain <sheriellaghzain@gmail.com>
2026-05-04 11:57:09 +0700

    menambahkan class mahasiswa

---
+++ constructorberparameter.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+public:
+    mahasiswa(int nim, string nama); // constructor dengan parameter
+};
+
+
+int main(){
+    
+}
