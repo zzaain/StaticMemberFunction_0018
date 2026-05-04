8f6a3937c7ed7ece863ba916c174f159d4fa66cb
zzaain <sheriellaghzain@gmail.com>
2026-05-04 12:05:29 +0700

    menambahkan constructor nama

--- constructoverloading.cpp
+++ constructoverloading.cpp
@@ -5,3 +5,8 @@ mahasiswa :: mahasiswa(){
 mahasiswa :: mahasiswa(int iNim){
     nim = iNim; // definisi hanya NIM
 }
+
+mahasiswa :: mahasiswa(string iNama){
+    nama = iNama; // definisi hanya NAMA
+}
+
